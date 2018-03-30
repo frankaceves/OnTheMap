ec880e87c43844ea23b10482d0b51368e3d80e46
Frank Aceves <frankaceves@gmail.com>
2018-03-29 19:33:47 -0700

    add initial Constants struct

---
+++ OnTheMap/Model/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  OnTheMap
+//
+//  Created by Frank Aceves on 3/29/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    
+    
+}
