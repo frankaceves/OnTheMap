b6a93cfa3c693aa4daeab1075c86b643a6116bbf
Frank Aceves <frankaceves@gmail.com>
2018-04-08 15:21:39 -0700

    add Parse client & constants info.

--- OnTheMap/Model/Parse Client/ParseClient.swift
+++ OnTheMap/Model/Parse Client/ParseClient.swift
@@ -10,4 +10,84 @@ import UIKit
 
 class ParseClient: NSObject {
 
+    struct StudentInformation {
+        let studentFirstName : String?
+        let studentLastName : String?
+        let studentLatitude : Double?
+        let studentlongitude : Double?
+        let locationMapString : String?
+        let studentMediaURL: String?
+        let studentObjectId : String
+        let studentUniqueKey : String
+        
+        init(dictionary: [String:AnyObject]) {
+            studentFirstName = dictionary[ParseClient.JSONResponseKeys.FirstName] as? String
+            studentLastName = dictionary[ParseClient.JSONResponseKeys.LastName] as? String
+            studentLatitude = dictionary[ParseClient.JSONResponseKeys.Latitude] as? Double
+            studentlongitude = dictionary[ParseClient.JSONResponseKeys.Longitude] as? Double
+            locationMapString = dictionary[ParseClient.JSONResponseKeys.MapString] as? String
+            studentMediaURL = dictionary[ParseClient.JSONResponseKeys.URL] as? String
+            studentObjectId = dictionary[ParseClient.JSONResponseKeys.ObjectID] as! String
+            studentUniqueKey = dictionary[ParseClient.JSONResponseKeys.UdacityID] as! String
+        }
+    }
+    
+    func getStudentInfo(_ completionHandlerfForGetStudentInfo: @escaping (_ result: [StudentInformation]?, _ error: NSError?) -> Void) {
+        //1.  set parameters
+        //2.  create url
+        //3.  configure request
+        var request1 = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation")!)
+        request1.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request1.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session1 = URLSession.shared
+        
+        //    4.  Make request (task) check for errors (error, data, 2xx statuscode)
+        let task1 = session1.dataTask(with: request1) { data, response, error in
+            // Handle error...
+            
+            guard (error == nil) else {
+                print("error in your request")
+                return
+            }
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                print("status code returned other than 2xx")
+                return
+            }
+            guard let data = data else {
+                print("no data returned")
+                return
+            }
+            //RAW JSON DATA
+            //print(String(data: data, encoding: .utf8)!)
+            
+            //    5.  parse data (deserialize json data)
+            let parsedResults: [String: AnyObject]!
+            
+            do {
+                parsedResults = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String:AnyObject]
+            } catch {
+                print("error parsing data")
+                return
+            }
+            
+            print(parsedResults)
+//            if let results = parsedResults["results"] as? [[String:AnyObject]] {
+//                let students = studentsFromResults(results)
+//                completionHandlerfForGetStudentInfo(students, nil)
+//            } else {
+//                completionHandlerfForGetStudentInfo(nil, NSError(domain: "student location", code: 0, userInfo: [NSLocalizedDescriptionKey:"could not parse student info into dictionary"]))
+//            }
+            
+            
+            
+            //    6.  use the data (assign data to local properties)
+            
+        }
+        //    7.  Start Request (task.resume)
+        task1.resume()
+        
+    }
+    
+    
+    
 }
---
+++ OnTheMap/Model/Parse Client/ParseConstants.swift
@@ -0,0 +1,22 @@
+//
+//  ParseConstants.swift
+//  OnTheMap
+//
+//  Created by Frank Aceves on 4/8/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import Foundation
+
+extension ParseClient {
+    struct JSONResponseKeys {
+        static let UdacityID = "uniqueKey"
+        static let FirstName = "firstName"
+        static let LastName = "lastName"
+        static let Latitude = "latitude"
+        static let Longitude = "longitude"
+        static let MapString = "mapString"
+        static let URL = "mediaURL"
+        static let ObjectID = "objectId"
+    }
+}
