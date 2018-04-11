5e1479a4ecac24253f454c6e9763d7c190f12fa3
Frank Aceves <frankaceves@gmail.com>
2018-04-10 21:34:16 -0700

    create StudentMapVC class file.

---
+++ OnTheMap/Controller/StudentMapViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StudentMapViewController.swift
+//  OnTheMap
+//
+//  Created by Frank Anthony Aceves on 4/10/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import UIKit
+
+class StudentMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
