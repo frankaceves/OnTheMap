7156311633f54709b6bcd4e1673a7caa3297d263
Frank Aceves <frankaceves@gmail.com>
2018-04-18 07:42:54 -0700

    add cancelPost function to dismiss postLocation VC

--- OnTheMap/Controller/PostLocationViewController.swift
+++ OnTheMap/Controller/PostLocationViewController.swift
@@ -21,7 +21,10 @@ class PostLocationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func cancelPosting(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
