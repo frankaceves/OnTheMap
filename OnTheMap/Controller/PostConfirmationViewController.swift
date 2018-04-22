920516aca322f84f153df7df1eaae11c7fb38ac9
Frank Aceves <frankaceves@gmail.com>
2018-04-21 18:23:27 -0700

    add PostConfirmation VC, make MKMapViewDelegate

---
+++ OnTheMap/Controller/PostConfirmationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PostConfirmationViewController.swift
+//  OnTheMap
+//
+//  Created by Frank Anthony Aceves on 4/21/18.
+//  Copyright © 2018 Frank Aceves. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class PostConfirmationViewController: UIViewController, MKMapViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
