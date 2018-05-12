af72d53c502862fd939b388135c3fe07faba69a4
Frank Aceves <frankaceves@gmail.com>
2018-05-12 15:09:24 -0700

    remove activity Indicator code from studentMap VC

--- OnTheMap/Controller/StudentMapViewController.swift
+++ OnTheMap/Controller/StudentMapViewController.swift
@@ -174,28 +174,4 @@ class StudentMapViewController: UIViewController, MKMapViewDelegate {
 
 }
 
-//extension UIViewController {
-//    class func activateSpinner(onView : UIView) -> UIView {
-//        let spinnerView = UIView.init(frame: onView.bounds)
-//        spinnerView.backgroundColor = UIColor.init(white: 0.10, alpha: 0.75)
-//
-//        let ai = UIActivityIndicatorView.init(activityIndicatorStyle: .whiteLarge)
-//
-//        ai.startAnimating()
-//        ai.center = spinnerView.center
-//
-//        DispatchQueue.main.async {
-//            spinnerView.addSubview(ai)
-//            onView.addSubview(spinnerView)
-//        }
-//
-//        return spinnerView
-//    }
-//
-//    class func deactivateSpinner(spinner :UIView) {
-//        DispatchQueue.main.async {
-//            spinner.removeFromSuperview()
-//        }
-//    }
-//}
 
